95bb7f7e4bc6539ff74d1785411b34c91285c621
Rejnszu <lukaszrejnsz@gmail.com>
2022-09-28 11:53:49 +0200

    some changes for client

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import smoothscroll from "./scrollPolyfill";
+
+// kick off the polyfill!
+smoothscroll.polyfill();
+
 const transition = document.querySelector(".transition");
 const optionWrappers = document.querySelectorAll(".option-wrapper");
 const options = document.querySelectorAll(".option");
@@ -10,6 +15,13 @@ const coursesDiv = document.querySelector('[data-site="courses"]');
 const currentStage = document.querySelector(".counter");
 const coursesPage = document.querySelector(".courses-page");
 const startingPage = document.querySelector(".starting-page");
+
+const choosenCourseinformationInput = document.getElementById(
+  "choosen-course-information-input"
+);
+const choosenWayinformationInput = document.getElementById(
+  "choosen-way-information-input"
+);
 const startSelectingCourses = document.querySelector(
   ".start-selecting-courses"
 );
@@ -28,22 +40,33 @@ function currentStageDescriptionHandler(option) {
   switch (option.dataset.choice) {
     case "korepetycje-z-informatyki":
       currentStageDescription.textContent = "W czym możemy ci pomóc?";
+      backOffPage.style.top = "70%";
       break;
     case "zwykly-formularz":
       currentStageDescription.textContent = "Skontaktuj się z nami.";
+      backOffPage.style.top = "90%";
       break;
     case "textarea-formularz":
       currentStageDescription.textContent = "Opisz jakiego kursu szukasz.";
+      backOffPage.style.top = "85%";
       break;
     case "kurs-maturalny":
       currentStageDescription.textContent = "Jaki tryb pracy ci pasuje?";
+      backOffPage.style.top = "70%";
       break;
     case "kurs-ambitny":
       currentStageDescription.textContent =
         "Jaka intensywność kursu cię interesuje?";
+      backOffPage.style.top = "80%";
+      break;
+    case "kurs-ambitny-grupowy":
+      currentStageDescription.textContent =
+        "Jaka intensywność kursu cię interesuje?";
+      backOffPage.style.top = "80%";
       break;
     case "mature-zdaje":
       currentStageDescription.textContent = "Kiedy zdajesz maturę?";
+      backOffPage.style.top = "70%";
       break;
   }
 }
@@ -54,20 +77,30 @@ function currentStageDescriptionReverse() {
   switch (currentOptionsDataSite) {
     case "courses":
       currentStageDescription.textContent = "Jaki jest twój cel?";
+      backOffPage.style.top = "70%";
       break;
 
     case "korepetycje-z-informatyki":
       currentStageDescription.textContent = "W czym możemy ci pomóc?";
+      backOffPage.style.top = "70%";
       break;
     case "kurs-maturalny":
       currentStageDescription.textContent = "jaki tryb pracy ci pasuje?";
+      backOffPage.style.top = "70%";
       break;
     case "mature-zdaje":
       currentStageDescription.textContent = "Kiedy zdajesz maturę?";
+      backOffPage.style.top = "70%";
       break;
     case "kurs-ambitny":
       currentStageDescription.textContent =
         "Jaka intensywność kursu cię interesuje?";
+      backOffPage.style.top = "80%";
+      break;
+    case "kurs-ambitny-grupowy":
+      currentStageDescription.textContent =
+        "Jaka intensywność kursu cię interesuje?";
+      backOffPage.style.top = "80%";
       break;
   }
 }
@@ -95,6 +128,7 @@ function checkIfReverseButtonIsDisplayed() {
 
 backOffPage.addEventListener("click", () => {
   formInputs.forEach((input) => (input.value = ""));
+
   backOffPage.setAttribute("disabled", true);
   additionalInformations.forEach((information) =>
     information.classList.remove("active")
@@ -123,6 +157,9 @@ backOffPage.addEventListener("click", () => {
     if (previousPageData === "kurs-ambitny") {
       previousPageData = "kurs-maturalny";
     }
+    if (previousPageData === "kurs-ambitny-grupowy") {
+      previousPageData = "kurs-maturalny";
+    }
     setTimeout(() => {
       options.forEach((option) => option.classList.remove("move-out"));
       backOffPage.removeAttribute("disabled");
@@ -130,6 +167,10 @@ backOffPage.addEventListener("click", () => {
       currentStageDescriptionReverse();
       counter--;
       currentStage.textContent = counter;
+
+      setTimeout(() => {
+        coursesPage.scrollIntoView({ behavior: "smooth" });
+      }, 400);
     }, 1);
   }, 300);
 });
@@ -149,30 +190,57 @@ function formHandler() {
           formTitle.textContent = "KURS INDYWIDUALNY";
           formDescription.innerHTML = "";
           formWorkTime.textContent = "";
+          choosenCourseinformationInput.value = "KURS INDYWIDUALNY";
+          choosenWayinformationInput.value = option.querySelector(
+            ".course-description"
+          ).textContent;
           break;
         case "sam-na-sam":
           formTitle.textContent = "KURS INDYWIDUALNY AMBITNY";
           formWorkTime.textContent = "60 minut / tydzień ";
           formDescription.innerHTML =
             "<ul><li>Praca domowa po każdych zajęciach</li> <li> Materiały podstawowe w cenie kursu</li> <li>Możliwość konsultacji między zajęciami</li> <li>Dostęp do prywatnej grupy szkolnej</li></ul>";
+          choosenCourseinformationInput.value = "KURS INDYWIDUALNY AMBITNY";
+          choosenWayinformationInput.value =
+            "Wolę pracować sam na sam z korepetytorem";
           break;
         case "sam-na-sam-plus":
           formTitle.textContent = "KURS INDYWIDUALNY AMBITNY PLUS";
           formWorkTime.textContent = "120 minut / tydzień ";
           formDescription.innerHTML =
             "<ul><li>Praca domowa po każdych zajęciach</li> <li>Dodatkowe prace domowe dla chętnych</li> <li>Materiały podstawowe i rozszerzone w cenie kursu</li> <li>Możliwość konsultacji między zajęciami</li> <li>Dostęp do prywatnej grupy szkolnej</li></ul>";
+          choosenCourseinformationInput.value =
+            "KURS INDYWIDUALNY AMBITNY PLUS";
+          choosenWayinformationInput.value =
+            "Wolę pracować sam na sam z korepetytorem";
           break;
-        case "kameralna-grupa":
+        case "kurs-ambitny-grupowy":
           formTitle.textContent = "KURS GRUPOWY AMBITNY";
           formWorkTime.textContent = "90 minut / tydzień";
           formDescription.innerHTML =
             "<ul><li>Praca domowa po każdych zajęciach <li>Dodatkowe prace domowe dla chętnych</li> <li>Materiały podstawowe i rozszerzone w cenie kursu</li> <li>Możliwość konsultacji między zajęciami</li> <li>Dostęp do prywatnej grupy szkolnej</li></ul>";
+          choosenCourseinformationInput.value = "KURS GRUPOWY AMBITNY";
+          choosenWayinformationInput.value =
+            "Wole pracować w kameralnej grupie";
+          break;
+        case "kurs-ambitny-grupowy-plus":
+          formTitle.textContent = "KURS GRUPOWY AMBITNY PLUS";
+          formWorkTime.textContent = "90 minut / tydzień";
+          formDescription.innerHTML =
+            "<ul><li>Praca domowa po każdych zajęciach <li>Dodatkowe prace domowe dla chętnych</li> <li>Materiały podstawowe i rozszerzone w cenie kursu</li> <li>Możliwość konsultacji między zajęciami</li> <li>Dostęp do prywatnej grupy szkolnej</li></ul>";
+          choosenCourseinformationInput.value = "KURS GRUPOWY AMBITNY PLUS";
+          choosenWayinformationInput.value =
+            "Wole pracować w kameralnej grupie";
           break;
         case "samodzielnie-w-domu":
           formTitle.textContent = "KURS AMBITNY WYKŁADOWY";
           formWorkTime.textContent = "120 minut / tydzień";
           formDescription.innerHTML =
             "<ul><li>Praca domowa po każdych zajęciach</li> <li>Dodatkowe prace domowe dla chętnych</li> <li>Materiały podstawowe i rozszerzone w cenie kursu</li> <li>Możliwość konsultacji między zajęciami</li> <li>Dostęp do prywatnej grupy szkolnej</li></ul>";
+          choosenCourseinformationInput.value = "KURS AMBITNY WYKŁADOWY";
+          choosenWayinformationInput.value = option.querySelector(
+            ".course-description"
+          ).textContent;
           break;
       }
     });
@@ -208,6 +276,10 @@ options.forEach((option) => {
         options.forEach((option) => option.classList.remove("move-out"));
         counter++;
         currentStage.textContent = counter;
+
+        setTimeout(() => {
+          coursesPage.scrollIntoView({ behavior: "smooth" });
+        }, 400);
       }, 500);
     }, 500);
   });
